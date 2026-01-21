8aa8b6a91446cddb224f9914659736383b3c9f68
Gaurav775-Git <gaurav775.official@gmail.com>
2026-01-22 01:53:37 +0530

    MVP of calender is ready

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Login from './loginpages/Login'
 import Social from './mainpages/Social'
 import {Router,Route,Routes} from 'react-router-dom'
+import TaskLog from './pages/TaskLog'
 const App = () => {
   return (
     <>
     <Routes>
       <Route path='/' element={<Login/>}/>
       <Route path='/social' element={<Social/>}/>
+      <Route path='/tasks' element={<TaskLog/>}/>
 
     </Routes>
     </>
