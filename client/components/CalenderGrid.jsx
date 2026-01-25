1a4af17325d2a456ffd7c113b6587c92e99dbc57
Gaurav775-Git <gaurav775.official@gmail.com>
2026-01-25 10:31:40 +0530

    heat map color fix

--- client/components/CalenderGrid.jsx
+++ client/components/CalenderGrid.jsx
@@ -1,22 +1,24 @@
 import React, { useState } from 'react'
 
 const CalenderGrid = ({ day, value = 'empty' }) => {
-  const colorMap = {
-    core: '#22D3EE',
-    good: '#22C55E',
-    neutral: '#FACC15',
-    bad: '#FB923C',
-    nightmare: '#EF4444',
-    empty: '#15181D'
+
+  const [color , setColor] = useState(false);
+  function heatColor(){
+    setColor(true);
   }
 
   return (
     <div>
-      <button className="w-7 h-7 sm:w-8 sm:h-8 lg:w-9 lg:h-9
-      rounded-lg flex items-center justify-center
-      text-[10px] sm:text-xs text-gray-300
-      hover:scale-105 transition cursor-pointer"
-      style={{ backgroundColor: colorMap[value] }}
+      <button className={`
+  w-7 h-7 sm:w-8 sm:h-8 lg:w-9 lg:h-9
+  rounded-lg flex items-center justify-center
+  text-[10px] sm:text-xs
+  hover:scale-105 transition cursor-pointer
+
+  ${color? "bg-green-500 text-white" : "bg-gray-800 text-gray-300"}`}
+
+  onClick={heatColor}
+
       >
         {day}
       </button>
