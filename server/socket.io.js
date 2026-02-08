be45acae997549cfa1ffaaf846c500e0c6d8c127
Gaurav Goswami <gaurav775.official@gmail.com>
2026-02-08 23:52:41 +0530

    Update CORS origin for socket.io server

--- server/socket.io.js
+++ server/socket.io.js
@@ -3,7 +3,7 @@ const { Server } = require("socket.io");
 const configureSocket = (server) => {
   const io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: "https://dailist-beta.vercel.app/",
       methods: ["GET", "POST"],
     },
   });
